fada53d44164b6be7e204ab8390b3e71842accab
Swidnixx <swidnixx@gmail.com>
2023-04-28 18:18:45 +0200

    Uzupelnilismy CameraController i zmodyfikowalismy, aby podlaczac auto w trakcie gry.Dodalismy playerSPawn i zaczelismy Menu

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,12 +4,30 @@ using Cinemachine;
 public class CameraController : MonoBehaviour
 {
     public Vector3[] positions;
-    public CinemachineVirtualCamera camera;
+    public CinemachineVirtualCamera vcamera;
     CinemachineTransposer transposer;
+    int currentPosition = 0;
 
     private void Start()
     {
-        transposer = camera.GetCinemachineComponent<CinemachineTransposer>();
+        transposer = vcamera.GetCinemachineComponent<CinemachineTransposer>();
         transposer.m_FollowOffset = positions[0];
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            currentPosition++;
+            currentPosition = currentPosition % positions.Length;
+
+            transposer.m_FollowOffset = positions[currentPosition];
+        }
+    }
+
+    public void SetCameraToCar(Transform car)
+    {
+        vcamera.Follow = car;
+        vcamera.LookAt = car;
     }
 }
---
+++ Assets/Scripts/RaceLogic/PlayerSpawner.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSpawner : MonoBehaviour
+{
+    public GameObject carPrefab;
+    public Transform[] spawnPos;
+    public int playerCount = 4;
+
+    private void Start()
+    {
+        for(int i=0; i<playerCount; i++)
+        {
+            var car = Instantiate(carPrefab, spawnPos[i].position, spawnPos[i].rotation);
+            if(i == 0)
+            {
+                car.GetComponent<PlayerController>().enabled = true;
+                GameObject.FindObjectOfType<CameraController>().SetCameraToCar(car.transform);
+            }
+        }
+    }
+}
